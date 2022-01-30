25b66e70e267764ee36e70a4a6e4cabf3cea71af
rcoon1 <78291341+rcoon1@users.noreply.github.com>
2022-01-30 09:10:06 -0600

    add

---
+++ Topic 1/topic1-1/src/main/java/com/gcu/Topic11Application.java
@@ -0,0 +1,14 @@
+package com.gcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Topic11Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from my Spring Boot Application");
+		SpringApplication.run(Topic11Application.class, args);
+	}
+
+}
