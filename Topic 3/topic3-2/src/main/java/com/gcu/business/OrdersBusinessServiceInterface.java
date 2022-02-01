feb954c61e9c6c33ea04319f27449b206508daae
rcoon1 <78291341+rcoon1@users.noreply.github.com>
2022-01-31 21:59:49 -0600

    Topic 3-2 Added

---
+++ Topic 3/topic3-2/src/main/java/com/gcu/business/OrdersBusinessServiceInterface.java
@@ -0,0 +1,17 @@
+package com.gcu.business;
+
+import java.util.List;
+
+import com.gcu.model.OrderModel;
+
+public interface OrdersBusinessServiceInterface {
+	
+	public void test();
+	
+	public List<OrderModel> getOrders();
+	
+	public void init();
+	
+	public void destroy();
+
+}
